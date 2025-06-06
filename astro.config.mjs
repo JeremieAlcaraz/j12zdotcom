a11e0c47781ed5315fa1ed498af223c8b028825c
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-06-06 20:28:13 +0200

    feat/add header feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), svelte(), mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
