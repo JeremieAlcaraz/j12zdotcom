1fa35b706be17f96cf2be7c2858db763fe2b0b15
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-06-21 19:32:03 +0200

    ✨ ContactForm component and contact page

---
+++ src/components/contact/index.ts
@@ -0,0 +1,10 @@
+// src/components/contact/index.ts
+// ------------------------------------------------------------------
+// Permet un import plus propre ailleurs dans le code :
+//
+//   import { ContactForm } from '@components/contact';
+//
+// ------------------------------------------------------------------
+
+export { default as ContactForm } from './ContactForm.astro';
+export type { ContactFormProps } from './ContactForm.astro';
