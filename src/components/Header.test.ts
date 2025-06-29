2e83801c531fab9388fd19f85670f87df8ed581a
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-06-29 09:51:38 +0200

    ♻️ Clean and reorganise the code

--- src/components/Header.test.ts
+++ src/components/Header.test.ts
@@ -1,7 +1,7 @@
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { expect, describe, it } from 'vitest';
 
-import Navbar from './Header.astro';
+import Navbar from '@components/Header.astro';
 
 const links = [
   { href: '/foo', label: 'Foo' },
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 // src/components/navbar/index.ts
 
 // 1) Le composant principal
-export { default as Navbar } from './Header.astro';
+export { default as Navbar } from '@components/Header.astro';
 
 // 2) Le type des props (optionnel mais souvent utile)
 export type { NavbarProps } from './Header.types';
